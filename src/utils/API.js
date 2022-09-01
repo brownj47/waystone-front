f08a344a16b7b73bf59d28aec41f88bdfcf825d0
Justus B <justus.c.brown@gmail.com>
2022-09-01 13:20:01 -0700

    token logic

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,15 @@
-const URL_PREFIX = 'http://localhost:3001/api';
+const URL_PREFIX = 'http://localhost:3001';
 
 const API = {
     checkToken:token=>{
-        return fetch(`${URL_PREFIX}/users/checkToken`,{
+        return fetch(`${URL_PREFIX}/checkToken`,{
             headers:{
                 'Authorization': `Bearer ${token}`
             }
         })
     },
     login: (email, password) => {
-        return fetch(`${URL_PREFIX}/users/login`, {
+        return fetch(`${URL_PREFIX}/login`, {
             method: 'POST',
             body: JSON.stringify({
                 email,
