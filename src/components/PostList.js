1a5bebe01e6eb7a8a7377d14fbaa7c9c28c2537a
Austin Gentz <austingentz@icloud.com>
2022-09-02 17:15:47 -0700

    added an add post feature and functionality

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,30 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import API from '../utils/API';
 import PostCard from './PostCard';
 
 function PostList() {
-	const posts = [
-		{ id: 1, content: 'post1' },
-		{ id: 2, content: 'post2' },
-		{ id: 3, content: 'post3' },
-		{ id: 4, content: 'post4' },
-		// More items...
-];
-// post hooks
-	const [allPosts, setAllPosts] = useState([])
-	useEffect(()=>{
-		API.getAllPosts().then(res=>res.json()).then(posts=>{
-			setAllPosts(posts)
-		})
-	}, [])
-	
+	// post hooks
+	const [allPosts, setAllPosts] = useState([]);
+	useEffect(() => {
+		API.getAllPosts()
+			.then((res) => res.json())
+			.then((posts) => {
+				setAllPosts(posts);
+			});
+	}, []);
+
 	return (
-		<div className="grid grid-cols-1 gap-4 lg:col-span-2">
+		<div className="grid grid-cols-1 gap-4 lg:col-span-2 overflow-auto">
 			{/* POST PANEL */}
 			<section aria-labelledby="quick-links-title">
-				<div className="bg-white w-full h-screen rounded-lg">
+				<div className="bg-white w-full  rounded-lg">
 					<ul role="list" className="divide-y divide-lime-400">
-						{allPosts.map((post) => (<PostCard {...post} key={post._id} />))}
+						{allPosts.map((post) => (
+							<PostCard {...post} key={post._id} />
+						))}
 					</ul>
 				</div>
 			</section>
