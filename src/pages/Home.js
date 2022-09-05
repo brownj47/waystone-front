339cb007aed4bbd0813f66a1f83fc6fe34771ac8
Austin Gentz <austingentz@icloud.com>
2022-09-05 11:25:44 -0700

    added create post to top. random friend popout buttton. next is linking the button to the popout.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -198,26 +198,15 @@ export function Home(props) {
 												</nav>
 											</div>
 											<div className="px-12 lg:px-0">
-												{/* Search */}
+												{/* Random Friend pop out button */}
 												<div className="mx-auto w-full max-w-xs lg:max-w-md">
-													<label htmlFor="search" className="sr-only">
-														Search
-													</label>
-													<div className="relative text-white focus-within:text-gray-600">
-														<div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-															<MagnifyingGlassIcon
-																className="h-5 w-5"
-																aria-hidden="true"
-															/>
-														</div>
-														<input
-															id="search"
-															className="block w-full rounded-md border border-transparent bg-lime-200 bg-opacity-20 py-2 pl-10 pr-3 leading-5 text-white placeholder-white focus:border-transparent focus:bg-opacity-100 focus:text-gray-900 focus:placeholder-gray-500 focus:outline-none focus:ring-0 sm:text-sm"
-															placeholder="Search"
-															type="search"
-															name="search"
-														/>
-													</div>
+													<button
+														type="button"
+														className="inline-flex w-full flex-shrink-0 items-center justify-center rounded-md border border-transparent hover:bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:flex-1"
+														onClick={<RandFriendPopOut />}
+													>
+														Random Friend
+													</button>
 												</div>
 											</div>
 										</div>
@@ -381,13 +370,14 @@ export function Home(props) {
 				<main className="-mt-24 pb-8 bg-gradient-to-r from-zinc-800 to-zinc-700">
 					<div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
 						<h1 className="sr-only">Profile</h1>
+						{<CreatePost user={userObj} />}
 						{/* Main 3 column grid */}
 						<div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8 overflow-auto ">
 							{/* Left column */}
 							<PostList />
 
 							{/* Right column */}
-							<div className="grid grid-cols-1 gap-4">
+							<div className="grid grid-cols-1 gap-4 m-2">
 								{userObj.friends ? (
 									<FriendsList friends={userObj.friends} />
 								) : (
@@ -398,7 +388,6 @@ export function Home(props) {
 								) : (
 									<></>
 								)}
-								{<CreatePost user={userObj} />}
 							</div>
 						</div>
 					</div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -140,7 +140,6 @@ export default function Login(props) {
 				</div>
 			</div>
 			{/* temporary */}
-			<RandFriendPopOut />
 		</>
 	);
 }
